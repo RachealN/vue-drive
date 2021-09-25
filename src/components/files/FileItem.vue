416db110140ad0ec5cd7ffbfe2cc628f4133dc0d
RachealN <rechealmatsikoit@gmail.com>
2021-09-25 19:57:42 +0300

    refactor filesitem to composition api

--- src/components/files/FileItem.vue
+++ src/components/files/FileItem.vue
@@ -1,20 +1,21 @@
 <template>
     <div class="col-md-3">
         <div class="card mb-4">
-          <img class="file-thumb" :src="isValidImage" v-if="file.url" />
+          <img class="file-thumb" :src="file.url" v-if="isValidImage" />
           <div class="card-body text-center py-5" v-else>
             <component :is="iconFileType" height="4em" width="4em" />
           </div>
           <div class="card-footer">
             <div class="d-flex align-items-center">
               <component :is="iconFileType" />
-              <span class="file-name">{{file.name}}</span>
+               <span class="file-name">{{file.name}}</span>
             </div>
           </div>
         </div>
       </div>
 </template>
 <script>
+import { computed }  from "vue"; 
 export default {
    props: {
        file: {
@@ -22,26 +23,26 @@ export default {
            required: true
        },
    },
-   
-   computed: {
-       iconFileType() {
-           const iconTypes = {
-               "video/mp4": "icon-type-video",
-                "image/jpeg": "icon-type-image",
-                "image/jpg": "icon-type-image",
-                "image/jng": "icon-type-image",
-                "application/zip": "icon-type-zip",
-                "application/msword": "icon-type-doc",
-                "application/vnd.ms-excel": "icon-type-excel",
-                "application/pdf": "icon-type-pdf",
-           };
-           return iconTypes[this.file.mimeType] ? iconTypes[this.file.mimeType] : "icon-type-common";
-       },
+   setup(props) {
+        const iconFileType = computed(() => {
+            const iconTypes = {
+                "video/mp4": "icon-type-video",
+                    "image/jpeg": "icon-type-image",
+                    "image/jpg": "icon-type-image",
+                    "image/jng": "icon-type-image",
+                    "application/zip": "icon-type-zip",
+                    "application/msword": "icon-type-doc",
+                    "application/vnd.ms-excel": "icon-type-excel",
+                    "application/pdf": "icon-type-pdf",
+            };
+            return iconTypes[props.file.mimeType] ? iconTypes[props.file.mimeType] : "icon-type-common";
+        });
 
-       isValidImage() {
-           const imageMimeTypes = [ 'image/jpg', 'image/jpeg', 'image/png'];
-           return imageMimeTypes.includes(this.file.mimeType) && this.file.url;
-       }
+        const isValidImage = computed(() => {
+            const imageMimeTypes = [ 'image/jpg', 'image/jpeg', 'image/png'];
+            return imageMimeTypes.includes(props.file.mimeType) && !!props.file.url;
+        })
+        return { iconFileType, isValidImage }
    }
-}
+};
 </script>
