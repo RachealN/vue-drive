940d3eb79c7b8ffebfa334133fb0c44b36d01ce2
RachealN <rechealmatsikoit@gmail.com>
2021-09-25 18:24:38 +0300

    create file item and files list

---
+++ src/components/files/FileItem.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="col-md-3">
+        <div class="card mb-4">
+          <img class="file-thumb" :src="isValidImage" v-if="file.url" />
+          <div class="card-body text-center py-5" v-else>
+            <component :is="iconFileType" height="4em" width="4em" />
+          </div>
+          <div class="card-footer">
+            <div class="d-flex align-items-center">
+              <component :is="iconFileType" />
+              <span class="file-name">{{file.name}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+<script>
+export default {
+   props: {
+       file: {
+           type:Object,
+           required: true
+       },
+   },
+   
+   computed: {
+       iconFileType() {
+           const iconTypes = {
+               "video/mp4": "icon-type-video",
+                "image/jpeg": "icon-type-image",
+                "image/jpg": "icon-type-image",
+                "image/jng": "icon-type-image",
+                "application/zip": "icon-type-zip",
+                "application/msword": "icon-type-doc",
+                "application/vnd.ms-excel": "icon-type-excel",
+                "application/pdf": "icon-type-pdf",
+           };
+           return iconTypes[this.file.mimeType] ? iconTypes[this.file.mimeType] : "icon-type-common";
+       },
+
+       isValidImage() {
+           const imageMimeTypes = [ 'image/jpg', 'image/jpeg', 'image/png'];
+           return imageMimeTypes.includes(this.file.mimeType) && this.file.url;
+       }
+   }
+}
+</script>
