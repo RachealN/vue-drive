ea08ec0a3f3bde95bd19332f18acea33e37e916f
RachealN <rechealmatsikoit@gmail.com>
2021-09-26 22:25:09 +0300

    add teleport for search form

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,28 +3,15 @@
     class="
       navbar navbar-light
       bg-white
-      navbar-expand-lg
+      navbar-expand-lg 
       border-bottom
       fixed-top
     ">
     <div class="container">
       <a class="navbar-brand" href="/"><strong>Vue</strong>Drive</a>
-      <search-form  v-model="q"/>
+      <!-- <search-form  v-model="q"/> -->
+      <div id="search-form"></div>
     </div>
   </nav>
 </template>
 
-<script>
-import SearchForm from './SearchForm.vue';
-import { ref, watchEffect } from 'vue';
-export default {
-  components: { SearchForm },
-  setup() {
-    const q = ref("");
-    
-    watchEffect(() => console.log('q', q.value));
-    
-    return { q };  
-  },
-}
-</script>
