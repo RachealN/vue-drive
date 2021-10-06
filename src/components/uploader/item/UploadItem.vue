3f6580f5d9f672f2570ead7b13edaa23f2fb79ff
RachealN <rechealmatsikoit@gmail.com>
2021-10-06 17:01:49 +0300

    add upload item component

---
+++ src/components/uploader/item/UploadItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li 
+        class="list-group-item d-flex justify-content-between align-items-center"> 
+        <p class="upload-item">{{ item.file.name}}</p>
+        <div class="upload-controls">X</div>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type:Object,
+            required: true
+        }
+    },
+}
+</script>
