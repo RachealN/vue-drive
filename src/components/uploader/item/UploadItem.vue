7ffe4d5fb0a3119b3c88f0bf33ef89a337152ad3
RachealN <rechealmatsikoit@gmail.com>
2021-10-06 17:24:53 +0300

    make icon type dynamic

--- src/components/uploader/item/UploadItem.vue
+++ src/components/uploader/item/UploadItem.vue
@@ -1,12 +1,16 @@
 <template>
     <li 
         class="list-group-item d-flex justify-content-between align-items-center"> 
-        <p class="upload-item">{{ item.file.name}}</p>
+        <p class="upload-item">
+            <component :is="iconFileType"/>
+            <span> {{ item.file.name}} </span>
+        </p>
         <div class="upload-controls">X</div>
     </li>
 </template>
 
 <script>
+import { useIconFileType } from '../../../composable/icon-file-type'
 export default {
     props: {
         item: {
@@ -14,5 +18,40 @@ export default {
             required: true
         }
     },
+    setup(props) {
+        return {
+            iconFileType: useIconFileType(props.item.file.type)
+        }
+
+    }
 }
 </script>
+
+<style scoped>
+ .upload-item {
+  line-height: 2;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin: 0;
+}
+
+.upload-item,
+.failed-text {
+  color: #6c757d;
+}
+
+.upload-item span,
+.failed-text {
+  font-size: 12px;
+}
+
+.upload-item span {
+  margin-left: 6px;
+}
+
+.failed {
+  width: 140px;
+  color: #212529;
+}
+</style>
